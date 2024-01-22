f54430fe2d278958cb4c8f7657534efd1069f086
santi0ne <johsrodr@espol.edu.ec>
2024-01-22 01:33:57 -0500

    barra y creacion de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PrincipalComponent } from './paginas/principal/principal.component';
+import { FacturaComponent } from './paginas/factura/factura.component';
+import { AcercadeComponent } from './paginas/acercade/acercade.component';
+
+const routes: Routes = [
+  { path: "principal", component: PrincipalComponent },
+  { path: "factura", component: FacturaComponent},
+  { path: "acercade", component: AcercadeComponent},
+  { path: "", redirectTo: '/main', pathMatch: 'full' },
+  { path: "**", redirectTo: "main" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {HttpClientModule} from '@angular/common/http';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { PrincipalComponent } from './paginas/principal/principal.component';
+import { FacturaComponent } from './paginas/factura/factura.component';
+import { AcercadeComponent } from './paginas/acercade/acercade.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    FacturaComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [
     provideClientHydration()
