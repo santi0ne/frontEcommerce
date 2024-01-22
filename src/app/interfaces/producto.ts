19ca7ca4476584f7dff16c5e8d56534bec7ca2d8
Jeremy Martinez <jjmartin@espol.edu.ec>
2024-01-22 15:25:14 -0500

    producto interface

---
+++ src/app/interfaces/producto.ts
@@ -0,0 +1,10 @@
+export interface Producto {
+    categoria: number;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    imagen: string;
+    created_at: Date;
+    updated_at: Date;
+    pid: number;
+}
