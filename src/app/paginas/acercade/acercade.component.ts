ea697a95ccc077837f5da1ff0769f9c0df8c72fb
nicmacor <nicmacor@espol.edu.ec>
2024-01-22 23:52:00 -0500

    Acerca de

--- src/app/paginas/acercade/acercade.component.ts
+++ src/app/paginas/acercade/acercade.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-acercade',
   templateUrl: './acercade.component.html',
-  styleUrl: './acercade.component.css'
+  styleUrls: ['./acercade.component.css']
 })
-export class AcercadeComponent {
 
+export class AcercadeComponent {
+  creadores = [
+    { nombre: 'Nicolas Coronel', rol: 'Developer', imagen: 'assets/Nicolas.jpeg' },
+    { nombre: 'Jeremy Martinez', rol: 'Developer', imagen: 'assets/Jeremy.jpg' },
+    { nombre: 'Santiago Rodriguez', rol: 'Developer', imagen: 'assets/Santiago.jpg' }
+  ];
 }
+
